624742c9a72d7b60100d2493b5b6c435736062e3
stebomurkn420 <stevn4127@gmail.com>
2019-01-16 12:06:45 -0500

    Add VoLTE and Permission hack

--- device-liquid.mk
+++ device-liquid.mk
@@ -13,6 +13,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
+#Permissons
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.control_privapp_permissions=log
+
+# VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
+
 # Update engine
 PRODUCT_PACKAGES += brillo_update_payload
 
